660ae51161ce978c779e4318af848623112769f7
JuanJoToAl <159054367+JuanJoToAl@users.noreply.github.com>
2024-04-24 20:01:53 -0500

    Add files via upload

--- Reto_10_punto_2.py
+++ Reto_10_punto_2.py
@@ -1,8 +1,8 @@
 # Se declaran e/o inicializan varaibles
-lista1: list = []
-lista2: list = []
-numero1: float
-numero2: float
+lista_1: list = []
+lista_2: list = []
+numero_1: float
+numero_2: float
 producto_punto: float = 0
 indice: int = 0
 longitud_lista: int
@@ -16,30 +16,30 @@ if longitud_lista < 0:
     
 else:
     # Se ejecuta bucle hasta longitud deseada en la lista
-    while len(lista1) < longitud_lista:
+    while len(lista_1) < longitud_lista:
         try:
-            numero1 = float(input("Ingrese un número entero para la lista 1"))
+            numero_1 = float(input("Ingrese un número entero para la lista 1"))
         except ValueError:
             print("Los caracteres ingresados no son válidos, intente de nuevo")
         
         # Se llena la lista1 con valores ingresados por el usuario
-        lista1.append(float(numero1))
-    print(f"Valores de lista1: {lista1}")
+        lista_1.append(float(numero_1))
+    print(f"Valores de lista1: {lista_1}")
 
     # Se ejecuta bucle hasta longitud deseada en la lista
-    while len(lista2) < longitud_lista:
+    while len(lista_2) < longitud_lista:
         try:
-            numero2 = float(input("Ingrese un número entero para la lista 2"))
+            numero_2 = float(input("Ingrese un número entero para la lista 2"))
         except ValueError:
             print("Los caracteres ingresados no son válidos, intente de nuevo")
         
         # Se llena la lista2 con valores ingresados por el usuario
-        lista2.append(float(numero2))
-    print(f"Valores de lista2: {lista2}")
+        lista_2.append(float(numero_2))
+    print(f"Valores de lista2: {lista_2}")
 
     # Calcular el producto punto de las listas
-    for n in lista1:
-        producto_punto += lista1[indice] * lista2[indice]
+    for n in lista_1:
+        producto_punto += lista_1[indice] * lista_2[indice]
         indice += 1
 
 # Mostrar el resultado del producto punto
